7d7d89a7c18c19393309bf446360fde667aa0dbe
yamagashi <90173762+yamagashi@users.noreply.github.com>
2021-10-10 15:55:12 +0900

    コミット

--- test/src/main/java/model/LoginLogic.java
+++ test/src/main/java/model/LoginLogic.java
@@ -1,5 +1,11 @@
 package model;
 
+//ログインに関する処理を行うモデル
+
 public class LoginLogic {
+	public boolean execute(User user) {
+		if(user.getPass().equals("1234")) {return true;}
+		return false;
+	}
 
 }
