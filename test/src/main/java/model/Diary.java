143b84496edcc065ee157d000fea4d8295cdf5b1
yamagashi <90173762+yamagashi@users.noreply.github.com>
2021-10-11 22:02:21 +0900

    kommit

--- test/src/main/java/model/Diary.java
+++ test/src/main/java/model/Diary.java
@@ -5,14 +5,17 @@ import java.io.Serializable;
 //文章に関する情報をもつJavaBeansモデル
 
 public class Diary implements Serializable {
+	private int id; //id
 	private String userName; //ユーザー名
 	private String text; //文章
 
 	public Diary() {}
-	public Diary(String userName, String text) {
+	public Diary(int id, String userName, String text) {
+		this.id = id;
 		this.userName = userName;
 		this.text = text;
 	}
+	public int getId() {return id; }
 	public String getUserName() {return userName;}
 	public String getText() {return text;}
 }
